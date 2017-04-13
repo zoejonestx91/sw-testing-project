a1dc9355a56fcf80282b21fe3efb8b0f5824bd41
tgduckworth <tgduckworth@gmail.com>
2017-04-12 22:20:33 -0500

    Refactored project to isolate CoverageTool from MetricsTool.

---
+++ metricstool/src/main/java/edu/utdallas/metricstool/tables/Column.java
@@ -0,0 +1,10 @@
+package edu.utdallas.metricstool.tables;
+
+/**
+ * Created by z on 4/10/17.
+ */
+public class Column {
+    public Column(String key, String name, MetricType type, String description){
+
+    }
+}
